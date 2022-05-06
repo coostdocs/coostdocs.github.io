f422f41b6775938dd37be2d646678a2b6c891a37
idealvin <idealvin@qq.com>
2022-05-06 12:47:23 +0800

    first commit

---
+++ upsearch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git rm --ignore-unmatch cn.search-data.min.* cn.search.min.* 
+git add cn.search-data.min.* cn.search.min.* 
+
+git rm --ignore-unmatch en.search-data.min.* en.search.min.*
+git add en.search-data.min.* en.search.min.*
